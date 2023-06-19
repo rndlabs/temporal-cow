81eaac4f05cf8eea3c10899a6d1e2a38864f663b
mfw78 <mfw78@protonmail.com>
2023-06-19 16:11:42 +0000

    test: coverage

--- test/TemporalCoW.t.sol
+++ test/TemporalCoW.t.sol
@@ -3,16 +3,51 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Test.sol";
 
-import {Base} from "composable/test/Base.t.sol";
+import {ChainlogHelper} from "dss-interfaces/dss/ChainlogAbstract.sol";
+import {BaseComposableCoWTest} from "composable/test/ComposableCoW.base.t.sol";
 
-contract TemporalCoWTest is Base {
+import "../src/TemporalCoW.sol";
 
-    TemporalCoW temporalCow;
+contract TemporalCoWTest is BaseComposableCoWTest, ChainlogHelper {
+    TemporalCow temporalCow;
 
     function setUp() public override {
         super.setUp();
+        temporalCow = new TemporalCow(settlement);
+    }
 
-        temporalCow = new TemporalCoW(settlement);
+    function testTemporalCow_RevertIfNotASolver() public {
+        vm.expectRevert(TemporalCow.NotSolver.selector);
+        temporalCow.flashloanAndSettle(new GPv2Interaction.Data[](0), GPv2Interaction.Data(address(0), 0, ""));
+    }
+
+    function testTemporalCow_RevertIfCallbackNotSet() public {
+        bytes memory cd = abi.encodeWithSignature("testtest()");
+        (bool success, bytes memory returnData) = address(temporalCow).call(cd);
+        assertEq(success, false);
+        assertEq(bytes4(returnData), TemporalCow.NoCallback.selector);
+    }
+
+    function testTemporalCow_CanBeCalledBySolver() public {
+        vm.prank(solver.addr);
+
+        IERC20[] memory tokens = new IERC20[](0);
+        uint256[] memory clearingPrices = new uint256[](0);
+        GPv2Trade.Data[] memory trades = new GPv2Trade.Data[](0);
+        GPv2Interaction.Data[][3] memory interactions =
+            [new GPv2Interaction.Data[](0), new GPv2Interaction.Data[](0), new GPv2Interaction.Data[](0)];
+
+        // second define the settlement interaction (this will be set as the callback)
+        GPv2Interaction.Data memory settlementInteraction = GPv2Interaction.Data({
+            target: address(settlement),
+            value: 0,
+            callData: abi.encodeCall(GPv2Settlement.settle, (tokens, clearingPrices, trades, interactions))
+        });
+
+        temporalCow.flashloanAndSettle(new GPv2Interaction.Data[](0), settlementInteraction);
+
+        // Ensure that the callback is no longer set
+        testTemporalCow_RevertIfCallbackNotSet();
     }
 
     function testTemporalCoW() public {
